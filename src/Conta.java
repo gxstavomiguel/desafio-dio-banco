a008c226d271271782667a5b8bccfdb2e3d1877a
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-09 23:21:56 -0300

    Trocar para interface

---
+++ src/Conta.java
@@ -0,0 +1,42 @@
+public class Conta extends IConta {
+
+
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+
+
+    public Conta(){
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
+
+    public void depositar(double valor){
+
+    }
+
+    public void sacar(double valor){
+
+    }
+
+    public void transferir(double valor, Conta contaDestino){
+
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+}
---
+++ src/IConta.java
@@ -0,0 +1,15 @@
+public class IConta  {
+
+    public void depositar(double valor){
+
+    }
+
+    public void sacar(double valor){
+
+    }
+
+    public void transferir(double valor, Conta contaDestino){
+
+    }
+
+}
